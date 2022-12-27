2e48a11673b36c09ab8872bf242058bb3effa857
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-12-28 01:27:27 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,29 @@ import css from './App.module.css';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
+    showModal: false,
   };
+
+  componentDidMount() {
+    const contactsList = localStorage.getItem('contacts');
+    const parsContacts = JSON.parse(contactsList);
+    if (parsContacts) {
+      this.setState({ contacts: parsContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addNewContact = (name, number) => {
     if (
@@ -33,6 +49,8 @@ export class App extends Component {
     this.setState(({ contacts }) => ({
       contacts: [newContact, ...contacts],
     }));
+
+    this.toggleModal();
   };
 
   deleteContact = contactId => {
@@ -46,20 +64,50 @@ export class App extends Component {
   };
 
   getNormaliseContacts = () => {
-    const normalisedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
+    const { filter, contacts } = this.state;
+    const normalisedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalisedFilter)
     );
   };
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
   render() {
-    const { filter } = this.state;
+    const { filter, contacts, showModal } = this.state;
     const filterName = this.getNormaliseContacts();
 
     return (
       <div>
         <h1 className={css.title}> Phonebook</h1>
+
+        <button
+         type="button"
+         onClick={this.toggleModal}
+         className={css.buttonModal}>
+          Open PhoneBook
+         </button>
+
+         {showModal && (
+          <Modal
+           onClose = {this.toggleModal}>
+            <h2 className={css.title}> Phonebook: </h2>
+            <Form  onSubmit={this.addNewContact}/>
+            Close
+            </button>
+            <p>
+               Please, enter name and number user</p>
+            
+         )};
+         </Modal>
+         )};
+
         <ContactForm onSubmit={this.addNewContact} />
+
         <h2 className={css.title}>Contacts</h2>
 
         <Filter value={filter} onChange={this.handleInputChange} />
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+// import css from '../Button.module.css';
+
+// const Button ({userToggleModal}) {
+//     type="button"
+//     onClick={userToggleModal}
+//     className ='css.modalButton'
+
+// };
